ab25c526f15d37e66bbcd3ac59f5fbf530dd6694
Kudaiberdi <kdajberdiz@gmail.com>
2025-11-30 10:56:56 +0500

    Check for date user token

--- src/main/java/kz/sdu/chat/mainservice/services/impl/MessageTokenServiceImpl.java
+++ src/main/java/kz/sdu/chat/mainservice/services/impl/MessageTokenServiceImpl.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -19,18 +21,14 @@ public class MessageTokenServiceImpl implements MessageTokenService {
 
     @Override
     public boolean CheckMessageToken(User user){
-
         UserTokens usertokens = userTokensRepository.findAllByUserId(user.getId());
-
-        if(usertokens == null){
+        CheckForDateUserToken(user);
+        if(usertokens == null) {
             CreateUserToken(user);
         }
-
-
         if(usertokens.getSpentCost() > 2){
             return false;
         }
-
         return true;
     }
 
@@ -41,26 +39,35 @@ public class MessageTokenServiceImpl implements MessageTokenService {
 
         if(usertokens == null){
             CreateUserToken(user);
-        }else{
-            usertokens.setSpentCost(usertokens.getSpentCost() + 1);
-            userTokensRepository.save(usertokens);
         }
 
-
+        usertokens.setSpentCost(usertokens.getSpentCost() + 1);
+        userTokensRepository.save(usertokens);
     }
 
     @Override
     public void CreateUserToken(User user) {
         UserTokens newUsertokens = new UserTokens();
         newUsertokens.setUserId(user.getId());
-        newUsertokens.setSpentCost(Long.valueOf(1));
+        newUsertokens.setSpentCost(Long.valueOf(0));
         newUsertokens.setUpdateDate(LocalDateTime.now());
         userTokensRepository.save(newUsertokens);
     }
 
     @Override
     public void CheckForDateUserToken(User user) {
+        UserTokens usertokens = userTokensRepository.findAllByUserId(user.getId());
 
+        if(usertokens == null){
+            CreateUserToken(user);
+        }
+
+        Duration duration = Duration.between(usertokens.getUpdateDate(), LocalDateTime.now());
+        log.info("Duration in days: {}", duration.toDays());
+        if(duration.toDays() >= 30){
+            usertokens.setSpentCost(Long.valueOf(0));
+            userTokensRepository.save(usertokens);
+        }
     }
 
 }
